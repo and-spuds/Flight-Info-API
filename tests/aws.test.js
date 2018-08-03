b9a358de561cc4aed046f4b23b93c5f2edfa8e48
Richard Merchant <richard@richinmedia.co.uk>
2018-08-03 13:05:17 +0100

    Update test description

--- tests/aws.test.js
+++ tests/aws.test.js
@@ -1,13 +1,13 @@
 const publishSNS = require("../app/lib/aws/aws").publishSNS;
 
 describe("AWS Publish", () => {
-  it("It throw TypeError when payload is empty", () => {
+  it("It should throw TypeError when payload is empty", () => {
     expect(() => {
       publishSNS(undefined);
     }).toThrow("payload is not an object");
   });
 
-  it("It throw Error when topicArn is empty", () => {
+  it("It should throw Error when topicArn is empty", () => {
     expect(() => {
       publishSNS({});
     }).toThrow("Topic ARN is not valid");
